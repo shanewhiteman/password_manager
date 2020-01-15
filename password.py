f05ce50e480d9fed9cdc07db7c1f3350f4e39b0a
Shane <Shanewhiteman99@gmail.com>
2020-01-15 14:21:43 -0500

    Update password.py

--- password.py
+++ password.py
@@ -1,1 +1,38 @@
-#try it!
+def account_create():
+  
+  return
+def security_questions():
+  
+  return
+def password_randomizer_query():
+  
+  return
+def random_pass():
+  
+  return
+def account_save():
+  
+  return
+def account_recovery():
+  
+  return
+def login():
+  
+  return
+def password_manager():
+  
+  return
+def password_save():
+  
+  return
+def password_rating(string):
+  
+  return
+def list_password():
+  
+  return
+def main():
+  
+  return
+
+main()
